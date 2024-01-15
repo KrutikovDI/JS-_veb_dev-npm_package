3e5f747879dc0d0e03a2fbd4db38706b06f2d88c
Danil Krutikov <kdanil.i@yandex.ru>
2024-01-15 20:46:42 +0700

    Third commit

--- src/app.js
+++ src/app.js
@@ -4,5 +4,5 @@ const characters = [
   {name: 'маг', health: 0},
   {name: 'лучник', health: 0},
 ];
-
-const alive = characters.filter(item => item.health > 0);
+characters.filter(item => item.health > 0);
+// const alive = characters.filter(item => item.health > 0);
